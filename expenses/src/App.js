44aeeb517a36c63e1e274ded9c2d51e569a8a14a
anasAB <anas.222@hotmail.com>
2021-07-05 07:42:28 +0200

    delet item and update the filter

--- expenses/src/App.js
+++ expenses/src/App.js
@@ -4,21 +4,21 @@ import ExpensesFilter from './Component/NewExpense/ExpensesFilter';
 import NewExpense from './Component/NewExpense/NewExpense';
 
 let Dummy_Expenses = [
-  { title: 'Car insurance', cost: 250, date: new Date(2021, 2, 2) },
-  { title: 'Health insurance', cost: 240, date: new Date(2021, 3, 4) },
-  { title: 'Rent', cost: 230, date: new Date(2021, 4, 3) },
-  { title: 'Car insurance', cost: 220, date: new Date(2021, 5, 2) },
-  { title: 'Books', cost: 100, date: new Date(2019, 6, 1) },
-  { title: 'Utilities', cost: 50, date: new Date(2019, 5, 1) },
-  { title: 'Home expense', cost: 210, date: new Date(2020, 6, 1) },
-  { title: 'Vacations', cost: 210, date: new Date(2020, 6, 1) },
-  { title: 'Vacations', cost: 210, date: new Date(2020, 6, 1) },
+  { id: 1, title: 'Car insurance', cost: 250, date: new Date(2021, 2, 2) },
+  { id: 2, title: 'Health insurance', cost: 240, date: new Date(2021, 3, 4) },
+  { id: 3, title: 'Rent', cost: 230, date: new Date(2021, 4, 3) },
+  { id: 4, title: 'Car insurance', cost: 220, date: new Date(2021, 5, 2) },
+  { id: 5, title: 'Books', cost: 100, date: new Date(2019, 6, 1) },
+  { id: 6, title: 'Utilities', cost: 50, date: new Date(2019, 5, 1) },
+  { id: 7, title: 'Home expense', cost: 210, date: new Date(2020, 6, 1) },
+  { id: 8, title: 'Vacations', cost: 210, date: new Date(2020, 6, 1) },
+  { id: 9, title: 'Vacations', cost: 210, date: new Date(2020, 6, 1) },
 ]
 
 
 const App = () => {
   const [expenses, setExpensesItem] = useState(Dummy_Expenses);
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('2021');
 
   //**! Add new Expenses */
   const receiveNewExpensive = (newExpensive) => {
@@ -26,48 +26,42 @@ const App = () => {
     setExpensesItem(newItem)
   }
 
-  //**! Delete Expenses */
-  const deleteExpense = (selectedExpense) => {
-    console.log('selectedExpense', selectedExpense);
-  }
-
-
   //**! Filter Expenses By Year*/
   const changeFilterHandler = (selectedYear) => {
-    setFilteredYear(selectedYear)
+    let filteredExpensesByYear = Dummy_Expenses.filter(item => {
+      return item.date.getFullYear().toString() === selectedYear
+    })
+    setExpensesItem(filteredExpensesByYear)
   }
 
-  let filterExpenses = Dummy_Expenses.filter(item => {
-    return item.date.getFullYear().toString() === filteredYear
-  })
+  //**! Delete Expenses */
+  const deleteExpense = (selectedExpense) => {
+    const deletedItem = expenses.filter((item) => item.id !== selectedExpense);
+    setExpensesItem(deletedItem)
+    console.log('deletedItem', expenses);
+  }
 
-  filterExpenses = filterExpenses.map(item => {
-    return (
-      <ExpenseItem key={Math.random()} title={item.title} cost={item.cost} date={item.date} amount={item.amount} deleteExpense={deleteExpense} />
-    )
-  })
 
-  
+
+
   //**! calculate total cost */
-  let totalCost = filterExpenses.reduce(function (accumulator, filterExpenses) {
-    return accumulator + filterExpenses.props.cost;
-  }, 0)
-
-
-  if (filterExpenses.length <= 0) {
-    filterExpenses = <div style={{ textAlign: 'center' }}><h1>There are no expenses right now </h1></div>
-  }
+  // let totalCost = expenses.reduce(function (accumulator, filterExpenses) {
+  //   return accumulator + filterExpenses.props.cost;
+  // }, 0)
 
 
   return (
     <div>
       <NewExpense receiveNewExpensive={receiveNewExpensive} />
       <ExpensesFilter selectedYear={filteredYear} onChangeFilter={changeFilterHandler} />
-      <div style={{marginLeft:'20px', fontSize:'20px',fontWeight:'bold'}}>TotalCost: {totalCost}</div>
-      {filterExpenses}
+      {/* <div style={{ marginLeft: '20px', fontSize: '20px', fontWeight: 'bold' }}>TotalCost: {totalCost}</div> */}
+      <ExpenseItem items={expenses} key={Math.random()} deleteExpense={deleteExpense} />
     </div>
   )
 
 }
 
 export default App;
+
+
+
