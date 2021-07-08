a7fc44810ed1c46b8069d74ad65e9026db622d02
anasAB <anas.222@hotmail.com>
2021-07-08 08:34:32 +0200

    delete Item

--- expenses/src/Component/ExpenseItem.js
+++ expenses/src/Component/ExpenseItem.js
@@ -5,40 +5,43 @@ import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
     const { items } = props
+    
 
+    //TODO
     const ChangeTitleHandler = (expense) => {
-        let inputVal = document.getElementById("title");
+        // let inputVal = document.getElementById("title");
     }
 
     const deleteExpenseItem = (deleteItem) => {
         props.deleteExpense(deleteItem);
     }
 
+
     return (
-        items.length > 0 ?
+        items.length >= 0 ?
         items.map(item => {
-            return (
-            <div className='expense-item' key={Math.random()} id={item.id}>
-            <ExpenseDate date={item.date} />
-            
-            <div className='expense-item__description'>
-                <h2>{item.title}</h2>
-                <div className='expense-item__price' >${item.cost}</div>
-            </div>
+                return (
+                    <div className='expense-item' key={Math.random()} id={item.id}>
+                        <ExpenseDate date={item.date} />
 
-            <div className='ex'>
-                <button className='expense-item__edit'
-                onClick={ChangeTitleHandler}>change title
-                </button>
+                        <div className='expense-item__description'>
+                            <h2>{item.title}</h2>
+                            <div className='expense-item__price' >${item.cost}</div>
+                        </div>
 
-                <button className='expense-item__delete'
-                onClick={() => deleteExpenseItem(item.id)}>Delete
-                </button>
-            </div>
-            </div>
-            )
-        })
-        : <p>Nothing</p>
+                        <div>
+                            <button className='expense-item__edit'
+                                onClick={ChangeTitleHandler}>change title
+                            </button>
+
+                            <button className='expense-item__delete'
+                                onClick={() => deleteExpenseItem(item.id)}>Delete
+                            </button>
+                        </div>
+                    </div>
+                )
+            })
+            : <p>Nothing</p>
     )
 
 
